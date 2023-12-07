03dde6d7a5e490b68b3b02dd950fc079331f90d5
ohnas <ohnas12@gmail.com>
2023-12-07 16:20:30 +0900

    Create Account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { auth } from "../firebase";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -42,6 +45,7 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+    const navigate = useNavigate();
     const [isLoading, setLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -59,13 +63,17 @@ export default function CreateAccount() {
             setPassword(value);
         }
     };
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if(isLoading || name === "" || email === "" || password === "") return;
         try {
-            // create an account
-            // set the name of the user.
-            // redirect to the home page
-            console.log(name, email, password)
+            setLoading(true);
+            const credentials = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(credentials.user);
+            await updateProfile(credentials.user, {
+                displayName: name,
+            });
+            navigate("/");
         } catch(e) {
             // setError(e);
         } finally {
@@ -74,7 +82,7 @@ export default function CreateAccount() {
     };
     return (
         <Wrapper>
-            <Title>Log into 𝕏</Title>
+            <Title>Join 𝕏</Title>
             <Form onSubmit={onSubmit}>
                 <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required />
                 <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
